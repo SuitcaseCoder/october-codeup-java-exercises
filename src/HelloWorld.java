afb2f0b5a424401116ca47626d5d6c503f36d35a
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-10-04 15:36:52 -0500

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+//this defines the class
+public class HelloWorld {
+    
+    // main method: entry point into our app
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
