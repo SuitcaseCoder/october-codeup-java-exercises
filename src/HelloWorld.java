edad1bdce45219c97ab9bcbd7da368729e99f56d
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-10-05 10:56:38 -0500

    syntax, types, variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,68 @@ public class HelloWorld {
     
     // main method: entry point into our app
     public static void main(String[] args){
+        // Intro to Java exercises
         System.out.println("Hello World!");
+        
+        // Syntax, Types, and Variables exercises:
+        // 1.
+        int myFavoriteNumber = 18;
+        System.out.println(myFavoriteNumber);
+        
+        // 2, 3, 4
+        String myString = "autumn is the best time of year";
+        System.out.println(myString);
+        
+        // 5, 6, 7, 8, 9
+        float myNumber = 123;
+        System.out.println(myNumber);
+        
+        // 10.
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+    
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        
+        // 11.
+//        String class = "not going to work because class is a special keyword";
+        
+        // 12.
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o; // this won't work
+//        int three = (int) "three";
+        
+        // 13.
+        int y = 4;
+        y++;
+        System.out.println(y);
+        
+        int z = 3;
+        int k = 4;
+        z *= k;
+        System.out.println(z);
+        
+        int v = 10;
+        int w = 2;
+        v /= w;
+        w -= v;
+        System.out.println(v);
+        System.out.println(w);
+        
+        // 14.
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
+
