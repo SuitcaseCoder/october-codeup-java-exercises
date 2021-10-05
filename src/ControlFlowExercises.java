91f9a0d8e2dbd8e80e196ad9021019d74db1ed26
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-10-05 16:34:59 -0500

    control flow exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,53 @@
+public class ControlFlowExercises {
+
+//    // 1. Loop Basics
+//    public static void main(String[] args) {
+//        // a. While
+////        int i = 5;
+////        while (i <= 15){
+////            System.out.println(i);
+////            i++;
+////        }
+//
+//        // b. do while
+////        int num = 0;
+////        do {
+////            num = num + 2;
+////            System.out.println(num);
+////        } while (num < 100);
+//
+////            int num = 100;
+////            do {
+////                num = num - 5;
+////                System.out.println(num);
+////            } while (num > -10);
+//
+////            long num = 2;
+////            do {
+////                num = num * num;
+////                System.out.println(num);
+////            } while (num < 100);
+//
+//        // c. For loop
+////        for(int i = 5; i <= 15; i++){
+////            System.out.println(i);
+////        };
+//
+////        for (long i = 2; i <= 1000000; i*=i){
+////            System.out.println(i);
+////        }
+
+//    }
+    
+    
+    // 2. Fizzbuzz
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0 &&  i % 5 == 0){
+                System.out.println();
+            }
+            
+        }
+    }
+    
+}
